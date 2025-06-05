ad418b73d73de2049632f1918bf820e8185c1fd8
Christian Cucco <cdc-96@hotmail.com>
2025-06-05 13:24:18 -0300

    Add files via upload

---
+++ hello-world.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Create a file called hello-world
+echo "Hello, World!" > hello-world
+
+# Confirm file creation
+echo "File 'hello-world' has been created with content:"
+cat hello-world
+
