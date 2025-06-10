bf8ad259d174c7d0731fe61d154cda550d2b588e
Christian Cucco <cdc-96@hotmail.com>
2025-06-10 16:57:51 -0300

    Update hello-world.sh

--- hello-world.sh
+++ hello-world.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create a file called hello-world
-echo "Hello, Amol!" > hello-world
+echo "Hello, World!" > hello-world
 
 # Confirm file creation
 echo "File 'hello-world' has been created with content:"
